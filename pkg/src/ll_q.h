b8d2b099b8c10cebf7c818f8ccd18d8609623198
kmfrick <kmfrick98@gmail.com>
2022-01-07 02:16:48 +0100

    Folder restructure

---
+++ pkg/src/ll_q.h
@@ -0,0 +1,34 @@
+
+extern "C" {
+#include <stdlib.h>
+#include <stdio.h>
+}
+
+#define CQ 3 // constante para definir dmax a prtir del rango intercuartilico
+
+class ll_q{
+	private :
+
+		typedef struct node{
+			float info;
+			node  *seg;
+		} node;
+
+		int npunts;
+
+		node *Topleft;
+		node *newTopright;
+		node *Topright;
+
+	public :
+
+		// constructoras
+		ll_q(int np);
+		~ll_q();
+		void add_ord(float info);
+
+		// consultoras
+
+		float dmax();
+
+};
